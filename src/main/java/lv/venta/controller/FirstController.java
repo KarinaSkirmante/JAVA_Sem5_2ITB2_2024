cd121191b011343fae5522ea16abb92545b7fe34
KarinaSkirmante <karina.krinkele@gmail.com>
2024-04-04 15:01:06 +0300

    Initial: Sem5

---
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -0,0 +1,26 @@
+package lv.venta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+	@GetMapping("/hello")//localhost:8080/hello
+	public String getHello() {
+		System.out.println("First Controller!!!");
+		return "hello-page"; //tiek parādīta hello-page.html lapa
+		
+	}
+	
+	@GetMapping("/hello/msg")//localhost:8080/hello/msg
+	public String getHelloMsg(Model model) {
+		System.out.println("Msg controller is called");
+		model.addAttribute("mydata", "Ziņa no JAVA Spring!!!!");
+		return "hello-msg-page";//tiek parādīta hello-msg-page.html lapa
+	}
+	
+	
+	
+}
