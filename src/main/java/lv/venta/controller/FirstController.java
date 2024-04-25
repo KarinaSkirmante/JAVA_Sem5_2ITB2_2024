1ec0c35a7cc15de6288f7461e741de2f4e7b4934
localuser <localuser@VEA.LV>
2024-04-25 14:47:22 +0300

    Optimise: Create filter controller class

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -26,9 +26,6 @@ public class FirstController {
 
 
 
-	@Autowired
-	private IFilterProductService filterService;
-
 	@GetMapping("/hello") // localhost:8080/hello
 	public String getHello() {
 		System.out.println("First Controller!!!");
@@ -64,90 +61,6 @@ public class FirstController {
 	}
 	
 
-	
-	
-
-	
-		
-	//TODO pameģinam ielikt atsevisķu nosaukumu html- kas tie ir pa produktiem
-	//un caur model nosūtīt so nosaukumu
-	//izveidojam 4 get kontrolierus prieks filtrācijas funkcijām
-	@GetMapping("/product/filter/price/{threshold}")//localhost:8080/product/filter/price/1.5
-	public String getProductFilterByPrice(@PathVariable("threshold") float threshold,
-			Model model) {
-		
-		try
-		{
-			ArrayList<Product> filterProducts 
-			= filterService.filterByPriceLessThanThreshold(threshold);
-			model.addAttribute("mydata", filterProducts);
-			model.addAttribute("msg", "Products filtered by price: " + threshold + " eur");
-			return "product-all-show-page";// tiek parādīta product-all-show-page.html lapa
-
-		}
-		catch (Exception e) {
-			model.addAttribute("errormsg", e.getMessage());
-			return "error-page";// tiek parādīta error-page.html lapa
-		}
-	
-	}
-	
-	
-	
-	//TODO uztaisām kontrolierus visām pārējām filterService funkcijām un notetsējam
-	@GetMapping("/product/filter/quantity/{threshold}")//localhost:8080/product/filter/quantity/20
-	public String getProductFilterByQuantity(@PathVariable("threshold") int threshold,
-			Model model) {
-		
-		try
-		{
-			ArrayList<Product> filterProducts 
-			= filterService.filterByQuantityLessThanThreshold(threshold);
-			model.addAttribute("mydata", filterProducts);
-			model.addAttribute("msg", "Products filtered by quantity: " + threshold );
-			return "product-all-show-page";// tiek parādīta product-all-show-page.html lapa
-
-		}
-		catch (Exception e) {
-			model.addAttribute("errormsg", e.getMessage());
-			return "error-page";// tiek parādīta error-page.html lapa
-		}
-	
-	}
-	
-	@GetMapping("/product/filter/text/{text}")//localhost:8080/product/filter/text/Sarkans
-	public String getProductFilterByText(@PathVariable("text") String text,
-			Model model) {
-		
-		try
-		{
-			ArrayList<Product> filterProducts 
-			= filterService.filterByTitleOrDescription(text);
-			model.addAttribute("mydata", filterProducts);
-			model.addAttribute("msg", "Products filtered by text: " + text );
-			return "product-all-show-page";// tiek parādīta product-all-show-page.html lapa
-
-		}
-		catch (Exception e) {
-			model.addAttribute("errormsg", e.getMessage());
-			return "error-page";// tiek parādīta error-page.html lapa
-		}
-	
-	}
-	
-	
-	@GetMapping("/product/calculate/total")//localhost:8080/product/calculate/total
-	public String getProductCaluclateTotal(Model model) {
-		try {
-			float result = filterService.calculateProductsTotalValue();
-			model.addAttribute("mydata", "Total value of all products is " + result + " eur");
-			return "hello-msg-page";// tiek parādīta hello-msg-page.html lapa
-		} catch (Exception e) {
-			model.addAttribute("errormsg", e.getMessage());
-			return "error-page";// tiek parādīta error-page.html lapa
-		}
-	}
-	
 	
 	
 }
